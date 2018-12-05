5bf9382c2767970e64c41b7b60b52951fecd7b56
Mawson Sammons <mawsonsammons@Mawsons-MacBook-Pro.local>
2018-12-05 14:21:10 +0800

    Initial run scipt

---
+++ Project/code/runScript.py
@@ -0,0 +1,7 @@
+#Runs shark from python, just a test to make sure I knew how
+# exists within the code directory at the highest level
+#imports
+
+import subprocess
+
+subprocess.call(["/Users/mawsonsammons/Documents/ICRARInternship/Project/code/shark/build/shark", "/Users/mawsonsammons/Documents/ICRARInternship/Project/code/shark/sample.cfg"])
