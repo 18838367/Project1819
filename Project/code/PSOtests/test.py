ccf7be9d68fc8833680bbd70aa59c5cf60dc86a0
Mawson Sammons <mawson.sammons@curtin.student.edu.au>
2019-01-24 07:37:00 +0000

    moving directories

--- Project/code/PSOtests/test.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-def caller(x, *args)
-	beta, eta, rho = x
-	xopt, fopt=subprocess.call(['python', '/Users/mawsonsammons/Documents/ICRARInternship/Project/code/PSOtests/myTestV2.py', beta, eta, rho])
-	return xopt, fopt
