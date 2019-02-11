95de777f44a0028deb97133d976ebad8d9e276b0
Mawson Sammons <mawson.sammons@curtin.student.edu.au>
2019-02-11 07:42:58 +0000

    changed how subvols allocation works

--- HPCCaller.py
+++ HPCCaller.py
@@ -1,35 +1,52 @@
-import numpy as np
-import psoHPC
-import subprocess
+#bread and butter
+import os
 import sys  
+import subprocess
+import multiprocessing
+import math
+import time
+import numpy as np
+#edited but based on public code
+import pso
+#developed internally
 import common
 import HIMF
 import stellarMF
 import stellarMF1
 import analysis
 import constraints
-import math
-import time
 import HPCCommon
-import os
-import multiprocessing
+
 ###########SWARM PARAMS#############
-ss = 4     ## swarmsize           #
-mi = 4    ## maximum iterations  #
-prc = 0     ## number of processes # 
+ss = 16     ## swarmsize           #
+mi = 10    ## maximum iterations  #
+prc = 0     ## number of processes, set to 0 if using shark submit #
 ####################################
 #############GLOBAL PARAMS########
 count=0
-def HPCCallSMF(x, *args):
+def HPCCallPSO(x, *args):
+	"""
+	- Handler function for running PSO on Shark on a SLURM based cluster. 
+	- Swarm size and number of iterations need to be set within the script for now
+	- Function needs the relative path to a Shark config file under the -c option
+	- For now the subprocess call within must be altered if you are changing shark submit options
+	- To find appropriate memory allocations peruse the initial output lines of each particle
+	"""
+	# this global count just tracks the number of iterations so they can be saved to different files
 	global count 
 	count = count + 1
+	subvols=''
 	sT=np.zeros([ss, 3])
-	modeldir, outdir, redshift_table, subvols, obsdir, GyrToYr, Zsun, XH, MpcToKpc, mlow, mupp, dm, mbins, xmf, imf, mlow2, mupp2, dm2, mbins2, xmf2, ssfrlow, ssfrupp, dssfr, ssfrbins, xssfr, statTest, MFOpt, zOpt, domainUP, domainLW = args
+	modeldir, outdir, redshift_table, subvolsList, obsdir, GyrToYr, Zsun, XH, MpcToKpc, mlow, mupp, dm, mbins, xmf, imf, mlow2, mupp2, dm2, mbins2, xmf2, ssfrlow, ssfrupp, dssfr, ssfrbins, xssfr, statTest, MFOpt, zOpt, domainUP, domainLW = args
 	space=np.genfromtxt('/home/msammons/Project1819/aux/searchSpace.txt', dtype='str')
+	
+	#this simply corrects an annoying python habit to put things into 1 D arrays if there is only 1 row in file
 	if space.size==4 :
 		temp=space
 		space=np.zeros([1,4], dtype='object_')
 		space[0,:]=temp
+	
+	#This file is read by the shark run/submit? script to determine which values Shark will be run for
 	f2=open('/home/msammons/Project1819/aux/particlePositions.ssv', 'w+')
 	for i in range(len(x[:,0])):
 		for j in range(len(space)):
@@ -41,18 +58,28 @@ def HPCCallSMF(x, *args):
 			
 		f2.write('\n')
 	f2.close()
-	subprocess.call(['./shark-submit', '-a', 'Pawsey0119', '-S', '../build/shark', '-w', '8:00', '-m', '1500M', '-c', '1', '-n', 'PSOSMF'+str(count), '-O', '/mnt/su3ctm/mawson/sharkOut/PSOoutput/PSOSMF'+str(count), '-E', '/home/msammons/Project1819/aux/particlePositions.ssv', '-V', '0 1', '../sample.cfg'])
+	#calls the submit process and hands in the correct arguments, due to the use of the common.parse_args they can't simply be moved to command line arguments
+	#perhaps they could be put in like a config file and read in. Anyway that may be a problem to solve another day
+	for i in range(len(subvolsList)):
+		subvols=str(subvols)+' '+str(subvolsList[i])
+			
+
+	subprocess.call(['./shark-submit', '-a', 'Pawsey0119', '-S', '../build/shark', '-w', '30:00', '-m', '1500M', '-c', '1', '-n', 'PSOSMF'+str(count), '-O', '/mnt/su3ctm/mawson/sharkOut/PSOoutput/PSOSMF'+str(count), '-E', '/home/msammons/Project1819/aux/particlePositions.ssv', '-V', subvols, '../sample.cfg'])
 	time.sleep(10)	
-	subvols="multiple_batches"
+	if(len(subvolsList)!=1):
+		subvols="multiple_batches"
+	else:
+		subvols=str(subvolsList[0])
 	#above will submit the shark instances for each PSO particle
 	#now need to ping queue until the jobs are done before returning the values 
 	while(HPCCommon.count_jobs('msammons')>0):
 		time.sleep(10)
 	
-	#####
-	#one ping, one ping only
-	######
+
+	#below performs the statistical computation of the "goodness" of fit
+
 	for i in range(ss):
+		#this directory will eventually need to be made generic, even more config file options is probably the best way forward
 		modeldir='/mnt/su3ctm/mawson/sharkOut/PSOoutput/PSOSMF'+str(count)+'/'+str(i)+'/mini-SURFS/my_model'
 		for j in range(len(MFOpt)):
 			xObs, xMod, yObs, yMod, ydn, yup=constraints.massFunction(modeldir, outdir, redshift_table, subvols, obsdir, GyrToYr, Zsun, XH, MpcToKpc, mlow, mupp, dm, mbins, xmf, imf, mlow2, mupp2, dm2, mbins2, xmf2, ssfrlow, ssfrupp, dssfr, ssfrbins, xssfr, MFOpt[j], zOpt[j])	
@@ -65,17 +92,23 @@ def HPCCallSMF(x, *args):
 
 
 if __name__ == '__main__':
+
+	modeldir, outdir, redshift_table, subvols, obsdir = common.parse_args()
+	#some parameters that the use needs to set
+	#################################
 	MFOpt=['HIMF', 'SMF', 'SMF']
 	zOpt=[0,0,1]
 	domainUP=[13,13,13]
 	domainLW=[7,8,8]
 	statTest='StudentT'
 	#statTest='Chi2'
+	subvolsList=[0]
+	#################################
+
 	if statTest == 'StudentT':
 		statTest = analysis.nonEqualStudentT
 	elif statTest == 'Chi2':
 		statTest = analysis.nonEqualChi2
-	modeldir, outdir, redshift_table, subvols, obsdir = common.parse_args()
 	##################################
 	# Constants
 	GyrToYr = 1e9
@@ -104,7 +137,7 @@ if __name__ == '__main__':
 	ssfrbins = np.arange(ssfrlow,ssfrupp,dssfr)
 	xssfr    = ssfrbins + dssfr/2.0
 
-	args=(modeldir, outdir, redshift_table, subvols, obsdir, GyrToYr, Zsun, XH, MpcToKpc, mlow, mupp, dm, mbins, xmf, imf, mlow2, mupp2, dm2, mbins2, xmf2, ssfrlow, ssfrupp,
+	args=(modeldir, outdir, redshift_table, subvolsList, obsdir, GyrToYr, Zsun, XH, MpcToKpc, mlow, mupp, dm, mbins, xmf, imf, mlow2, mupp2, dm2, mbins2, xmf2, ssfrlow, ssfrupp,
 		 	dssfr, ssfrbins, xssfr, statTest, MFOpt, zOpt, domainUP, domainLW)
 	space=np.genfromtxt('/home/msammons/Project1819/aux/searchSpace.txt')
 	if(space.size==4):
@@ -117,7 +150,7 @@ if __name__ == '__main__':
 		lb=space[:,2]
 	tStart=time.time()
 	os.chdir('../shark/hpc')
-	xopt, fopt=psoHPC.pso(HPCCallSMF, lb, ub, args=args, swarmsize=ss, maxiter=mi, processes=prc) 
+	xopt, fopt=pso.pso(HPCCallPSO, lb, ub, args=args, swarmsize=ss, maxiter=mi, processes=prc) 
 	tEnd=time.time()
 	print('xopt: ', xopt, 'fopt: ', fopt)
 	f=open('/mnt/su3ctm/mawson/sharkOut/PSOoutput/results/SMFri123_'+str(ss)+'_'+str(mi)+'.log', 'a+')
